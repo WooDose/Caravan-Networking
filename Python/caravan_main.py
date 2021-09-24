0810e9b8f7fccc022a533bc57e8561ae0d6e78fe
WooDose <59982861+WooDose@users.noreply.github.com>
2021-09-23 22:46:34 -0600

    Update caravan_main.py

--- Python/caravan_main.py
+++ Python/caravan_main.py
@@ -19,7 +19,7 @@ state_definition = {"pid" : "name",
 ## If Direction Flag is None, allow any numeric card to be played. Otherwise, only allow cards that go in said direction or letters. Reverse on Q.
 
 ## This is the game state that is actually used
-game_state = {1 : "Player 1",
+GAME_STATE = {1 : "Player 1",
               "player1" :
                   {1: ["1: The Hub",0,[], None],
                    2 : ["2: Junktown",0,[], None]}
@@ -39,7 +39,23 @@ game_state = {1 : "Player 1",
 
 net = Network()
 
+def get_winning_player():
+    playerwin = {'player1': 0,
+                 'player2': 0,
+                 'player3': 0}
+    for player in GAME_STATE:
+        if player != 'winning_round' and player not in [1,2,3]:
+            print(player)
+            for post in GAME_STATE[player]:
+                score = GAME_STATE[player][post][1]
+                if score >= 19 and score <= 25:
+                    playerwin[player] += 1
 
+    for wins in playerwin:
+        if playerwin[wins] == 2:
+            return (True, playerwin)
+
+    return (False, 0) 
 
 def calc_post_score(postcards):
     score = 0
@@ -50,46 +66,46 @@ def calc_post_score(postcards):
     return score
 
 def play_card(player, post, card):
-    if game_state[player][post][3] is None:
+    if GAME_STATE[player][post][3] is None:
         #add card to post    
         ##If it's the second card, determine direction, if it's the first, ignore
-        if len(game_state[player][post][2]) == 2:
-            print(game_state[player][post][2][-1], card)
-            if game_state[player][post][2][-1] == card:
+        if len(GAME_STATE[player][post][2]) == 2:
+            print(GAME_STATE[player][post][2][-1], card)
+            if GAME_STATE[player][post][2][-1] == card:
                 
                 print("Played card is the same as last card in this post. Card has to be different.")
             print("I am doing direction!!!")
-            direction = True if game_state[player][post][2][0] < game_state[player][post][2][1] else False
-            game_state[player][post][3] = direction
-            game_state[player][post][2].append(card)
+            direction = True if GAME_STATE[player][post][2][0] < GAME_STATE[player][post][2][1] else False
+            GAME_STATE[player][post][3] = direction
+            GAME_STATE[player][post][2].append(card)
         else:
-            game_state[player][post][2].append(card)
+            GAME_STATE[player][post][2].append(card)
 
-        print(type(game_state[player][post][1]))
-        game_state[player][post][1] = calc_post_score(game_state[player][post][2])
-        #pprint.pprint(game_state)
+        print(type(GAME_STATE[player][post][1]))
+        GAME_STATE[player][post][1] = calc_post_score(GAME_STATE[player][post][2])
+        #pprint.pprint(GAME_STATE)
         
-    elif game_state[player][post][3]:
-        if game_state[player][post][2][-1] == card:
+    elif GAME_STATE[player][post][3]:
+        if GAME_STATE[player][post][2][-1] == card:
             print("Played card is the same as last card in this post. Card has to be different.")
         #Allow any card higher than the currently placed
-            if card <= game_state[player][post][2][-1]:
+            if card <= GAME_STATE[player][post][2][-1]:
                 print("Post is going in increasing direction, card is decreasing")
             else:
-                game_state[player][post][2].append(card)
-                game_state[player][post][1] = calc_post_score(game_state[player][post][2])
-                #pprint.pprint(game_state)
+                GAME_STATE[player][post][2].append(card)
+                GAME_STATE[player][post][1] = calc_post_score(GAME_STATE[player][post][2])
+                #pprint.pprint(GAME_STATE)
             
-    elif not game_state[player][post][3]:
-        if game_state[player][post][2][-1] == card:
+    elif not GAME_STATE[player][post][3]:
+        if GAME_STATE[player][post][2][-1] == card:
             print("Played card is the same as last card in this post. Card has to be different.")
         #Allow any card higher than the currently placed
-            if card >= game_state[player][post][2][-1]:
+            if card >= GAME_STATE[player][post][2][-1]:
                 print("Post is going in increasing direction, card is decreasing")
             else:
-                game_state[player][post][2].append(card)
-                game_state[player][post][1] = calc_post_score(game_state[player][post][2])
-                #pprint.pprint(game_state)
+                GAME_STATE[player][post][2].append(card)
+                GAME_STATE[player][post][1] = calc_post_score(GAME_STATE[player][post][2])
+                #pprint.pprint(GAME_STATE)
 def send_data(target, post, card):
     data = str(net.id) + ":" + str(target) + "," + str(post) + "," + str(card)
 
@@ -137,7 +153,7 @@ while True:
             print("Una vez que se coloca una carta, solo se pueden colocar cartas mayores si la siguiente es mayor, o menores si la siguiente es menor.")
             print("El jugador gana si tiene un valor entre 19 y 25 para ambos puestos.")
             print("  ")
-            pprint.pprint(game_state)
+            pprint.pprint(GAME_STATE)
             player_input = input("Player, Target Player, Post, Card (Separated by spaces)\n")
             #player_input = "1,2,1,5"
             ##Get input
@@ -154,7 +170,7 @@ while True:
             
             player = int(player)
             #print(target,post)
-            #print(game_state[target][post])
+            #print(GAME_STATE[target][post])
             
             ## Verify if card is playable, else tell player to try another card. This should be defined on client side so it shouldn't happen.
             play_card(target,post,card)
@@ -173,7 +189,9 @@ while True:
 
             if tN2 != "" and pN2 != 0 and cN2 != "":
                 play_card(tN2 , pN2, cN2)
-        
+        if get_winning_player()[0] == True:
+            msg = "Message from" + usrName + ": " + usrName + "won the game!"
+            send_message(msg)
         if opcion == "2":
             msg = input("Ingrese el mensaje:\n")
             
