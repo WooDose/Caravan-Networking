592cb339152fcb4cd185e9e4d56a0ac9994cc2a0
DaronP <jandresperez1@yahoo.com>
2021-09-23 23:56:31 -0600

    Update caravan_main.py

--- Python/caravan_main.py
+++ Python/caravan_main.py
@@ -66,7 +66,7 @@ def calc_post_score(postcards):
     return score
 
 def play_card(player, post, card):
-    if GAME_STATE[player][post][3] is None:
+    if GAME_STATE[player+1][post][3] is None:
         #add card to post    
         ##If it's the second card, determine direction, if it's the first, ignore
         if len(GAME_STATE[player][post][2]) == 2:
